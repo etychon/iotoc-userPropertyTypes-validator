ab89aef418f604db0d635a0a324e0926018d06c1
etychon <etychon@gmail.com>
2020-09-30 17:17:55 +0200

    more verbose XML falure (with line + col indication)

--- validate_xml.py
+++ validate_xml.py
@@ -1,22 +1,34 @@
 import argparse
 import json
+import sys
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="input XML file (ie: userPropertyTypes.xml)")
 args = parser.parse_args()
+tree = '';
 
-tree = ET.parse(args.file)
+try:
+    tree = ET.parse(args.file)
 
-for elem in tree.iter():
+    for elem in tree.iter():
 
-    if elem.text[0] == '[':
-        print ("**JSON** %s: '%s'" % (elem.tag, elem.text))
-        try:
-            json.loads(elem.text)
-            print ("**JSON OK** %s: '%s'" % (elem.tag, elem.text))                                                      
-        except:
-            print ("**JSON ERROR** %s: '%s'" % (elem.tag, elem.text))
-    else:
-        print ("%s: '%s'" % (elem.tag, elem.text))
+        if elem.text[0] == '[':
+            print ("**JSON** %s: '%s'" % (elem.tag, elem.text))
+            try:
+                json.loads(elem.text)
+                print ("**JSON OK** %s: '%s'" % (elem.tag, elem.text))                                                      
+            except:
+                print ("**JSON ERROR** %s: '%s'" % (elem.tag, elem.text))
+        else:
+            print ("%s: '%s'" % (elem.tag, elem.text))
+
+except ParseError as p:
+    print("** XML ERROR** \"" + str(p.msg) + "\"")
+    
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    exit
         
